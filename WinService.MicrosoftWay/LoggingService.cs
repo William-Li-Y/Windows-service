b57dc9357f43927b73222f1b2187c5d06a9e82d1
WilliamLi <lixunjia@shinetechchina.com>
2020-02-14 21:23:03 +0800

    Inital

---
+++ WinService.MicrosoftWay/LoggingService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ServiceProcess;
+using System.Text;
+
+namespace WinService.MicrosoftWay
+{
+    class LoggingService : ServiceBase
+    {
+        private const string _logFileLocation = @"C:\temp\servicelog.txt";
+
+
+        private void Log(string message)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(_logFileLocation));
+            File.AppendAllText(_logFileLocation, DateTime.UtcNow.ToString()+":"+message+Environment.NewLine);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Log("Starting");
+            base.OnStart(args);
+        }
+
+        protected override void OnStop()
+        {
+            Log("Ending");
+            base.OnStop();
+        }
+
+        protected override void OnPause()
+        {
+            Log("Pausing");
+            base.OnPause();
+        }
+    }
+}
---
+++ WinService.MicrosoftWay/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace WinService.MicrosoftWay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBase.Run(new LoggingService());
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
